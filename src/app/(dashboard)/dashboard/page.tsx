f53d62a81fc15d1e8f1b89bcf0f4ba2435461c9e
sathwik <bnvsathwik75@gmail.com>
2023-06-21 13:39:10 +0530

    Final

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -21,13 +21,13 @@ const page = async () => {
   })
 
   return (
-    <div className='max-w-7xl mx-auto mt-16'>
-      {apiKey ? (
-        <ApiDashboard />
-      ) : (
-        <RequestApiKey />
-      )}
-    </div>
+    // <div className='max-w-7xl mx-auto mt-16'>
+    //   {apiKey ? (
+    //     <ApiDashboard />
+    //   ) : (
+    //     <RequestApiKey />
+    //   )}
+    // </div>
   )
 }
 
